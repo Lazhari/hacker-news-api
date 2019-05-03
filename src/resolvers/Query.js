c35b6ec134206cd2be5959b282e71f2497990d8c
Lazhari <lazhari.mohammed@outlook.com>
2019-05-03 21:04:27 +0100

    Removed irrelevant queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,32 +1,11 @@
-const os = require("os");
-
-const info = () => os.hostname();
-const osInfo = () => {
-  const userInfo = os.userInfo();
-  const cpus = os.cpus().length || 0;
-  const arch = os.arch();
-  const freemem = os.freemem();
-  const totalmem = os.totalmem();
-  const usedMemoryPercent = ((totalmem - freemem) / totalmem) * 100;
-  return {
-    ...userInfo,
-    cpus,
-    arch,
-    freemem,
-    totalmem,
-    usedMemoryPercent
-  };
-};
-const feed = (root, args, context, info) => {
+function feed(root, args, context, info) {
   return context.prisma.links();
-};
-const link = (parent, { id }, context, info) => {
+}
+function link(parent, { id }, context, info) {
   return context.prisma.link(id);
-};
+}
 
 module.exports = {
-  info,
-  osInfo,
   feed,
   link
 };
